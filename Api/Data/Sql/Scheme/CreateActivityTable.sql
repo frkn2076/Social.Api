87f5c5d2a1fede0c98c265f9965e848dcc93051b
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2022-11-08 22:38:10 +0300

    column size fix

--- Api/Data/Sql/Scheme/CreateActivityTable.sql
+++ Api/Data/Sql/Scheme/CreateActivityTable.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE IF NOT EXISTS activity (
     id serial PRIMARY KEY,
     title VARCHAR (100) NOT NULL,
-    detail VARCHAR (100) NOT NULL,
+    detail VARCHAR (1000) NOT NULL,
     location VARCHAR(100) NULL,
     date timestamp default NULL
 );
