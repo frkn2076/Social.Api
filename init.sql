c106300fd7d2cce7f8a2547aebff73abe47ccf17
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2022-11-27 23:45:13 +0300

    extended photo property size

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS public.profile (
     username VARCHAR (100) UNIQUE NULL,
     name VARCHAR (50) NULL,
     password VARCHAR (100) NOT NULL,
-    photo VARCHAR(100000) NULL,
+    photo VARCHAR(1000000) NULL,
     about VARCHAR(1000) NULL,
     role VARCHAR(10) NOT NULL
 );
